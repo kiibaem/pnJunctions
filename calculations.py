b8f0b35e70b5235365745594c460ca6115fb51ee
kiibaem <cba.larkin@gmail.com>
2018-03-12 14:42:13 +0000

    Now plots lnI vs V, and calculates n and I_0.

--- calculations.py
+++ calculations.py
@@ -1,5 +1,8 @@
 import numpy as np 
 import pandas as pd
+import scipy.optimize as op
+import dataSanitiser as ds
+import matplotlib.pyplot as plt
 
 e = 1.602e-19
 kB = 1.3806e-23
@@ -19,24 +22,51 @@ slopeErrs = [2.92e-4, 0.00168, 3.557e-4]
 icepts = [-16.63342, -6.32938, -12.59333]
 iceptErrs = [0.30161, 0.18847, 0.20177]
 
-names = ['GaAs(RT)', 'Ge(RT)', 'Si(RT)']
+names = ['GaAs(RT)', 'Ge(RT)', 'Si(RT)'] #79.5K -N
 
-def n(slopes):
-    ns = [(e)/(i*kB*T*1000) for i in slopes]
+def f(volts_s, m, c):
+    vals = m*volts_s + c
+    return vals
+
+def fitNplot(f,amps,volts,range):
+    lnamps = np.log(amps)
+    lnamps_s = lnamps[range[0]:range[1]]
+    volts_s = volts[range[0]:range[1]]
+    fit = op.curve_fit(f, volts_s, lnamps_s)
+    line = [fit[0][0]*i+fit[0][1] for i in volts_s]
+    plt.plot(volts_s, lnamps_s, 'o')
+    plt.plot(volts_s,line, '-')
+    perr = np.sqrt(np.diag(fit[1]))
+    val_err = nerr(fit[0][0],perr[0])
+    print("Value of n: %g" % n(fit[0][0]), "Error on n: %g" % val_err, "Value of I_0: %g A" % i0(fit[0][1]), "Error on I_0: %g" % i0err(fit[0][1],perr[1]))
+
+def n(slope):
+    ns = (e)/(slope*kB*T)
     return ns
 
-def nerr(slopes,slopeErrs):
-    nerrs = [np.sqrt((e/(slopes[i]*kB*T**2*1000))*sigmaT**2 + (e/(slopes[i]**2*kB*T*1000)*slopeErrs[i]**2)) for i in range(len(slopes))]
+def nerr(slope,slopeErr):
+    nerrs = np.sqrt((e/(slope*kB*T**2))*sigmaT**2 + (e/(slope**2*kB*T)*slopeErr**2))
     return nerrs
 
-def i0(icepts):
-    i0s = [np.exp(i) for i in icepts]
+def i0(icept):
+    i0s = np.exp(icept)
     return i0s
 
-def i0err(icepts,iceptErrs):
-    i0errs = [np.exp(icepts[i])*iceptErrs[i] for i in range(len(icepts))]
+def i0err(icept,iceptErr):
+    i0errs = np.exp(icept)*iceptErr
     return i0errs
 
-RTframe = pd.DataFrame({'I_0': pd.Series(i0(icepts), names), 'I_0 err': pd.Series(i0err(icepts,iceptErrs),names),'n': pd.Series(n(slopes), names), 'n err': pd.Series(nerr(slopes,slopeErrs),names)})
+def val_range(volts, lo, hi):
+    lo_i = 0
+    hi_i = 0
+    for i,j in enumerate(volts):
+        if j < lo:
+            lo_i = i
+        if j < hi:
+            hi_i = i
+    return [lo_i,hi_i]
 
-print(RTframe)
+#RTframe = pd.DataFrame({'I_0': pd.Series(i0(icepts), names), 'I_0 err': pd.Series(i0err(icepts,iceptErrs),names),'n': pd.Series(n(slopes), names), 'n err': pd.Series(nerr(slopes,slopeErrs),names)})
+
+fitNplot(f,ds.ampAvgList,ds.voltAvgList,val_range(ds.voltAvgList, 0.66, 1.1)) 
+plt.show()
